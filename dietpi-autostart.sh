862901b89d390db8849b2621f823b3ded3d3cdad
antonioavelar <sr.berquo@gmail.com>
2024-09-06 16:17:57 +0100

    feat: add copy of custom.sh diet-pi autostart

---
+++ dietpi-autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# DietPi-AutoStart custom script
+# Location: /var/lib/dietpi/dietpi-autostart/custom.sh
+
+
+curl -L https://raw.githubusercontent.com/antonioavelar/homelab/main/docker-compose.yml | docker compose -f >
+
+exit 0
